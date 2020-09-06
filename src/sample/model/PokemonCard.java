81812ef9f0ce974fb40c5e0f5982327a1cae98df
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-05 23:04:01 -0300

    Class PokemonCard.java updated with vars

--- src/sample/model/PokemonCard.java
+++ src/sample/model/PokemonCard.java
@@ -1,4 +1,13 @@
 package sample.model;
 
+import sample.enums.PokemonCardRarity;
+
 public class PokemonCard {
+    private String url;
+    private String id;
+    private String name;
+    private PokemonCardRarity rarity;
+    private String series;
+    private String set;
+
 }
