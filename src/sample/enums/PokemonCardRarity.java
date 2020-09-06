9516907fdf6462a543d4ab4e4b59e464a8b024ed
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-05 23:02:00 -0300

    Enum PokemonCardRarity.java updated with values

--- src/sample/enums/PokemonCardRarity.java
+++ src/sample/enums/PokemonCardRarity.java
@@ -1,5 +1,21 @@
 package sample.enums;
 
 public enum PokemonCardRarity {
-        
+    COMMON("Common"),
+    UNCOMMON("Uncommon"),
+    RARE("Rare"),
+    RARE_HOLO("Rare holo"),
+    RARE_HOLO_EX("Rare holo EX"),
+    RARE_ULTRA("Rare Ultra");
+
+
+    private String display;
+    PokemonCardRarity(String display) {
+        this.display = display;
+    }
+
+    @Override
+    public String toString() {
+        return display;
+    }
 }
