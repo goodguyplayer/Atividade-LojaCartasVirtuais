9a0a5894bba493a8c89542cc71b9fd4630572896
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-05 22:12:52 -0300

    DAO.java updated with basic info

--- src/sample/dao/DAO.java
+++ src/sample/dao/DAO.java
@@ -1,4 +1,12 @@
 package sample.dao;
 
-public class DAO {
+import java.util.List;
+
+public interface DAO <T>{
+    List<T> get(String condition);
+    List<T> getAll();
+    void update(T t);
+    void delete(T t);
+    void create(T t);
+    
 }
