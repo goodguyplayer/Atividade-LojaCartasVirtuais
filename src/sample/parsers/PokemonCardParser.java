22b4dc05203d73f954b6f61adec5f8b2264fa51c
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-07 10:37:57 -0300

    Class PokemonCardParser.java updated - toJson and fromJson

--- src/sample/parsers/PokemonCardParser.java
+++ src/sample/parsers/PokemonCardParser.java
@@ -1,4 +1,28 @@
 package sample.parsers;
+import org.json.JSONObject;
+import sample.model.PokemonCard;
 
 public class PokemonCardParser {
+    public static JSONObject toJson(PokemonCard card){
+        JSONObject json = new JSONObject();
+        json.put("id", card.getId());
+        json.put("name", card.getName());
+        json.put("imageUrl", card.getUrl());
+        json.put("rarity", card.getRarity());
+        json.put("series", card.getSeries());
+        json.put("set", card.getSet());
+        return json;
+    }
+
+    public static PokemonCard fromJson(JSONObject json){
+        PokemonCard card = new PokemonCard(
+                json.getString("id"),
+                json.getString("name"),
+                json.getString("imageUrl"),
+                PokemonCardRarityParser.toStatus(json.getString("rarity")),
+                json.getString("series"),
+                json.getString("set")
+        );
+        return card;
+    }
 }
