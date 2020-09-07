4d0124b58e5e577ac3d9b4c06614ba18ab017975
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-07 10:19:08 -0300

    Class PokemonClass.java updated - Getter And Setters

--- src/sample/model/PokemonCard.java
+++ src/sample/model/PokemonCard.java
@@ -10,4 +10,52 @@ public class PokemonCard {
     private String series;
     private String set;
 
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PokemonCardRarity getRarity() {
+        return rarity;
+    }
+
+    public void setRarity(PokemonCardRarity rarity) {
+        this.rarity = rarity;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public String getSet() {
+        return set;
+    }
+
+    public void setSet(String set) {
+        this.set = set;
+    }
 }
