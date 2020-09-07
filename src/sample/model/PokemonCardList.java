428ff4eb6139520e598afb47b5732225faa0ed8b
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-07 12:05:25 -0300

    Class PokemonCardList.java updated

--- src/sample/model/PokemonCardList.java
+++ src/sample/model/PokemonCardList.java
@@ -1,4 +1,27 @@
 package sample.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PokemonCardList {
+    private List<PokemonCard> cards;
+
+    public PokemonCardList() {
+        this.cards = new ArrayList<>();
+    }
+
+    public void addCard(PokemonCard card){
+        this.cards.add(card);
+    }
+
+    @Override
+    public String toString() {
+        return "PokemonCardList{" +
+                "cards=" + cards +
+                '}';
+    }
+
+    public final List<PokemonCard> getJogos() {
+        return this.cards;
+    }
 }
