8192584c9a055258cb9c00b739c80a4d53f4833f
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-07 10:57:30 -0300

    Class PokemonCardDAO.java updated - method get() rewritten

--- src/sample/dao/PokemonCardDAO.java
+++ src/sample/dao/PokemonCardDAO.java
@@ -1,17 +1,47 @@
 package sample.dao;
 
 import sample.model.PokemonCard;
+import sample.parsers.PokemonCardRarityParser;
 
-import java.sql.Connection;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PokemonCardDAO implements DAO<PokemonCard>, DAOFields {
     private Connection connection;
     private String myDBConnectionString = "jdbc:sqlite:pokemoncards.db";
 
+    public PokemonCardDAO(){
+        try {
+            connection = DriverManager.getConnection(myDBConnectionString);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
     @Override
     public List<PokemonCard> get(String condition) {
-        return null;
+        List<PokemonCard> cards = new ArrayList<>();
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(getSelectConditionalString(getTableName()) + condition);
+            while(result.next()){
+                PokemonCard card = new PokemonCard(
+                        result.getString("url"),
+                        result.getString("id"),
+                        result.getString("name"),
+                        PokemonCardRarityParser.toStatus(result.getString("rarity")),
+                        result.getString("series"),
+                        result.getString("set")
+                );
+                cards.add(card);
+            }
+            result.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return cards;
     }
 
     @Override
