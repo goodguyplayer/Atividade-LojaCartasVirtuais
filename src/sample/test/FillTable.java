18be9ed771fdabebcbc9cc3137d495879787f7ea
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-07 11:58:31 -0300

    Class FillTable.java created - Test

---
+++ src/sample/test/FillTable.java
@@ -0,0 +1,6 @@
+package sample.test;
+
+// Making a test class to both read Json and fill the database.
+// Mostly to avoid doing it all manually. I know Json is not in the exercise list, so I'll probably remove it later
+public class FillTable {
+}
