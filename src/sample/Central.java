93e444599a32ffb6e3820ecca438fecb7685ef7d
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-07 20:27:26 -0300

    Class Central.java moved and rewritten

---
+++ src/sample/Central.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Central extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 600));
+        primaryStage.show();
+    }
+
+    public void runMe(String[] args){
+        launch(args);
+
+    }
+}
