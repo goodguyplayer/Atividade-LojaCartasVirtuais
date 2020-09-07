e0f6ec3f08d613801bfd5fde462fab3ad9913b8b
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-07 10:42:27 -0300

    Interface DAOFields.java updated

--- src/sample/dao/DAOFields.java
+++ src/sample/dao/DAOFields.java
@@ -1,4 +1,10 @@
 package sample.dao;
 
 public interface DAOFields {
+    String getTableName();
+    String getDeleteString(String table);
+    String getUpdateString(String table);
+    String getInsertString(String table);
+    String getSelectAllString(String table);
+    String getSelectConditionalString(String table);
 }
