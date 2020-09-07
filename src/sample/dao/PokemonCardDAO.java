c9b630c7df0245c15f10e38d9092b5986911db01
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-07 11:03:36 -0300

    Class PokemonCardDAO.java update - method getAll() rewitten

--- src/sample/dao/PokemonCardDAO.java
+++ src/sample/dao/PokemonCardDAO.java
@@ -46,8 +46,29 @@ public class PokemonCardDAO implements DAO<PokemonCard>, DAOFields {
 
     @Override
     public List<PokemonCard> getAll() {
-        return null;
+        List<PokemonCard> cards = new ArrayList<>();
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(getSelectAllString(getTableName()));
+            while(result.next()){
+                PokemonCard card = new PokemonCard(
+                        result.getString("url"),
+                        result.getString("id"),
+                        result.getString("name"),
+                        PokemonCardRarityParser.toStatus(result.getString("rarity")),
+                        result.getString("series"),
+                        result.getString("set")
+                );
+                cards.add(card);
+            }
+            result.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return cards;
     }
+    
 
     @Override
     public void update(PokemonCard pokemonCard) {
