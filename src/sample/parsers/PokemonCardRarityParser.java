1b9b8151e7260d9284d8bd8b090f87558d636fe8
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-07 10:25:05 -0300

    Class PokemonCardRarityParser.java updated with rarity enums

--- src/sample/parsers/PokemonCardRarityParser.java
+++ src/sample/parsers/PokemonCardRarityParser.java
@@ -1,4 +1,14 @@
 package sample.parsers;
 
+import sample.enums.PokemonCardRarity;
+
 public class PokemonCardRarityParser {
+    public static PokemonCardRarity toStatus(String rarity){
+        if(rarity.equals("Uncommon")) return PokemonCardRarity.UNCOMMON;
+        if(rarity.equals("Rare")) return PokemonCardRarity.RARE;
+        if(rarity.equals("Rare holo")) return PokemonCardRarity.RARE_HOLO;
+        if(rarity.equals("Rare holo EX")) return PokemonCardRarity.RARE_HOLO_EX;
+        if(rarity.equals("Rare Ultra")) return PokemonCardRarity.RARE_ULTRA;
+        else return PokemonCardRarity.COMMON;
+    }
 }
