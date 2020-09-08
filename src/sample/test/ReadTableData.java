f2dcb408c2ada2c12f88f255af6e11ce1fb0aafb
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-08 08:31:36 -0300

    Class ReadTableData.java updated

--- src/sample/test/ReadTableData.java
+++ src/sample/test/ReadTableData.java
@@ -1,4 +1,28 @@
 package sample.test;
 
+import sample.dao.PokemonCardDAO;
+import sample.model.PokemonCard;
+
+/**
+ * <h1> Class ReadTableData</h1>
+ * <h3>Made for test</h3>
+ * Originally made to test the PokemonCardDAO's data gathering and possibly optimize PokemonCardDAO
+ *
+ * @author Nathan Brito da Silva - 17.00531-0
+ * @version 1.0
+ * @since 2020-09-08
+ */
 public class ReadTableData {
+    public static void main(String[] args) {
+        PokemonCardDAO cardDAO = new PokemonCardDAO();
+        System.out.println("All registered pokemons.");
+        for (PokemonCard card: cardDAO.getAll()) {
+            System.out.println("-------------------------");
+            System.out.println("Pokemon.: " + card.getName());
+            System.out.println("Set.: " + card.getSet());
+            System.out.println("Url.: " + card.getUrl());
+            System.out.println("ID.: " + card.getId());
+        }
+        System.out.println("-------------------------");
+    }
 }
