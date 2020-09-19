94ffb4eece2654a42db1c60e78d3865bac5d34c4
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-19 13:28:09 -0300

    class Controller.java updated - Added button methods

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,8 @@
 package sample;
 
 import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -11,8 +13,23 @@ import sample.model.PokemonCard;
 import sample.parsers.PokemonCardRarityParser;
 
 public class Controller {
+
     @FXML
-    public void buttonClicked(Event e){
+    public void buttonAdd(Event e){
         System.out.println("Button clicked");
     }
+
+    @FXML
+    public void buttonEdit(Event e){
+        System.out.println("Button clicked");
+    }
+
+    @FXML
+    public void buttonRemove(Event e){
+        System.out.println("Button clicked");
+    }
+
+    
+
+
 }
